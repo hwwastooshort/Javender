396e21108bbdcc8188b33980cb3ac7d02000ed5f
hw <molle.hw@gmail.com>
2025-01-14 13:25:03 +0100

    add custom Exception for DataManager

---
+++ src/main/java/Model/Database/DataManagerException.java
@@ -0,0 +1,11 @@
+package Model.Database;
+
+public class DataManagerException extends Exception {
+    public DataManagerException() {
+
+    }
+
+    public DataManagerException(String message) {
+        super(message);
+    }
+}
