546fce6252620a708ee4bfcc91af01e354a95b33
lisa <lisaperrin18@gmail.com>
2025-01-28 00:17:48 +0100

    Update main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,12 @@ public class Main {
     public static void main(String[] args) {
         clearScreen();
         System.out.println("This is from the main method!");
+
+        CalendarController controller = new CalendarController();
+        System.out.println("Welcome to Javender!");
+
+        controller.mainMenu();
+
     }
 
     public static void clearScreen() {
---
+++ src/main/java/View/MainMenuView.java
@@ -0,0 +1,32 @@
+package View;
+
+public class MainMenuView {
+    private final UserInterface ui;
+
+    public MainMenuView(UserInterface ui) {
+        this.ui = ui;
+    }
+
+    public void displayMainMenu() {
+        ui.displayMessage("\n=== Main Menu ===");
+        ui.displayMessage("1. Add appointment");
+        ui.displayMessage("2. Edit appointment");
+        ui.displayMessage("3. Remove appointment");
+        ui.displayMessage("4. Add tag");
+        ui.displayMessage("5. Edit tag");
+        ui.displayMessage("6. Exit");
+    }
+
+    public int getUserChoice() {
+        ui.displayMessage("Enter your choice: ");
+        return ui.getIntegerInput();
+    }
+
+    public void displayExitMessage() {
+        ui.displayMessage("Closing the application.");
+    }
+
+    public void displayInvalidChoiceMessage() {
+        ui.displayMessage("Invalid choice. Try again.");
+    }
+}
